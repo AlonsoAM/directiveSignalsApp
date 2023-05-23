d9eac1c7f4cc8b600402ae99431c754e93652214
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-05-23 13:01:08 -0500

    MAnejo de errores y propiedades computadas

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal, OnInit } from '@angular/core';
+import { Component, inject, signal, OnInit, computed } from '@angular/core';
 import { UsersServiceService } from '../../services/usersService.service';
 import { User } from '../../interfaces/user-request.interface';
 
@@ -14,6 +14,11 @@ export class UserInfoPageComponent implements OnInit {
   public currentUser = signal<User | undefined>(undefined);
   public userWasFound = signal(true);
 
+  public fullName = computed<string>(() => {
+    if (!this.currentUser) return 'Usuario no encontrado';
+    return `${this.currentUser()?.first_name} ${this.currentUser()?.last_name}`;
+  });
+
   ngOnInit(): void {
     this.loadUser(this.userID());
   }
@@ -22,8 +27,15 @@ export class UserInfoPageComponent implements OnInit {
     if (id <= 0) return;
     this.userID.set(id);
     this.currentUser.set(undefined);
-    this.usersService
-      .getUserByID(id)
-      .subscribe((user) => this.currentUser.set(user));
+    this.usersService.getUserByID(id).subscribe({
+      next: (value) => {
+        this.currentUser.set(value);
+        this.userWasFound.set(true);
+      },
+      error: () => {
+        this.userWasFound.set(false);
+        this.currentUser.set(undefined);
+      },
+    });
   }
 }
